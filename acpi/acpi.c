4d0efb9320433b45d20a4ffbd79750f91d0e0591
0xqoob <slad0716@gmail.com>
2020-06-01 20:52:13 -0700

    acpi table parsing

--- acpi/acpi.c
+++ acpi/acpi.c
@@ -1,5 +1,58 @@
 #include <acpi/acpi.h>
 
-void init_acpi(uint64_t rsdp) {
-	;	
-}	
+static struct rsdp_t* rsdp = NULL;
+static struct rsdt_t* rsdt = NULL;
+static struct xsdt_t* xsdt = NULL;
+
+void* find_sdt(const char* signature, int idx) {
+	int cnt = 0;
+
+	if (xsdt != NULL) {
+		for (size_t i = 0; i < (xsdt->sdt.len - sizeof(struct sdt_t)) / 8; i++) {
+			struct sdt_t* ptr = (struct sdt_t *)((size_t)xsdt->sdt_ptr[i] + HIGH_VMA);
+
+			if (!strncmp(ptr->signature, signature, 4)) {
+				if (cnt++ == idx) {
+					serial_printf("[ACPI] Found \"%s\" at %X\n", signature, (size_t)ptr);
+					return (void *)ptr;
+				}
+			}
+		}
+	} else {
+		for (size_t i = 0; i < (rsdt->sdt.len - sizeof(struct sdt_t)) / 4; i++) {
+			struct sdt_t* ptr = (struct sdt_t *)((size_t)rsdt->sdt_ptr[i] + HIGH_VMA);
+
+			if (!strncmp(ptr->signature, signature, 4)) {
+				if (cnt++ == idx) {
+					serial_printf("[ACPI] Found \"%s\" at %X\n", signature, (size_t)ptr);
+					return (void *)ptr;
+				}
+			}
+		}
+	}
+
+	serial_printf("[ACPI] Table with signature \"%s\" not found\n", signature);
+	return NULL;
+}
+
+void init_acpi(uint64_t rsdp_addr) {
+	if (rsdp_addr) {
+		rsdp = (struct rsdp_t *)rsdp_addr;
+		goto found;
+	} else {
+		serial_printf("[ACPI] Non-compliant system!\n");
+	}
+
+	return;
+
+found:
+	if (rsdp->rev >= 2 && rsdp->xsdt_paddr) {
+        serial_printf("[ACPI] Found XSDT at %X\n", ((size_t)rsdp->xsdt_paddr + HIGH_VMA));
+        xsdt = (struct xsdt_t *)((size_t)rsdp->xsdt_paddr + HIGH_VMA);
+    } else {
+        serial_printf("[ACPI] Found RSDT at %X\n", ((size_t)rsdp->rsdt_paddr + HIGH_VMA));
+        rsdt = (struct rsdt_t *)((size_t)rsdp->rsdt_paddr + HIGH_VMA);
+    }
+
+	init_madt();
+}
--- acpi/madt.c
+++ acpi/madt.c
@@ -1,18 +1,52 @@
 #include <acpi/madt.h>
 
-struct madt_entry_header {
-    uint32_t    lapic_addr;
-    uint32_t    flags;
+struct madt_t {
+    struct sdt_t sdt;
+    uint32_t local_controller_paddr;
+    uint32_t flags;
+    uint8_t madt_entries_begin;
 } __attribute__((packed));
 
-struct madt_entry_lapic {
-    uint8_t     acpi_proc_id;
-    uint8_t     apic_id;
-    uint32_t    flags; // bit 0: Processor Enabled | bit 1: Online Capable
+struct madt_header_t {
+    uint8_t type;
+    uint8_t len;
 } __attribute__((packed));
 
-struct madt_entry_1 {
-    uint8_t     ioapic_id;
-    uint8_t     reserved;
-    uint32_t    ioapic_addr;
+/* Type 0: LAPIC */
+struct madt__lapic_t {
+    struct madt_header_t header;
+    uint8_t acpi_proc_id;
+    uint8_t apic_id;
+    uint32_t flags; // bit 0: Processor Enabled | bit 1: Online Capable
 } __attribute__((packed));
+
+/* Type 1: IOAPIC */
+struct madt_ioapic_t {
+    struct madt_header_t header;
+    uint8_t ioapic_id;
+    uint8_t reserved;
+    uint32_t ioapic_addr;
+} __attribute__((packed));
+
+/* Type 2: Interrupt Source Override */
+struct madt_iso {
+    struct madt_header_t header;
+    uint8_t bus_src;
+    uint8_t irq_src;
+    uint32_t gsi;
+    uint16_t flags;
+} __attribute__((packed));
+
+/* Type 4: Non-maskable Interrupt */
+struct madt_nmi {
+    struct madt_header_t header;
+    uint8_t acpi_proc_id;
+    uint16_t flags;
+    uint8_t lint;
+} __attribute__((packed));
+
+static struct madt_t* madt = NULL;
+
+void init_madt() {
+	madt = find_sdt("APIC", 0);
+}
