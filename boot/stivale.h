3af064942a0062e8f18245bcb1ac1adfc3ec68de
404meemr <slad0716@gmail.com>
2020-06-01 18:37:04 -0700

    fixed crash

--- boot/stivale.h
+++ boot/stivale.h
@@ -9,36 +9,31 @@
 #define STIVALE_MEMORY_NVS                    4
 #define STIVALE_MEMORY_BADRAM                 5
 
-struct stivale_module {
-    uint64_t begin;   // Address where the module is loaded
-    uint64_t end;     // End address of the module
-    char string[128]; // String passed to the module (by config file)
-    uint64_t next;    // Pointer to the next module (if any), check module_count
-                      // in the stivale_struct
-} __attribute__((packed));
+typedef struct {
+    uint64_t begin;
+    uint64_t end;
+    char     string[128];
+} __attribute__((packed)) stivale_module_t;
 
-struct mmap_entry {
-    uint64_t base;   // Base of the memory section
-    uint64_t length; // Length of the section
-    uint32_t type;   // Type (described below)
+typedef struct {
+    uint64_t addr;
+    uint64_t len;
+    uint32_t type;
     uint32_t unused;
-} __attribute__((packed));
+} __attribute__((packed)) mmap_entry_t;
 
-struct stivale_info {
-    uint64_t cmdline;            // Pointer to a null-terminated cmdline
-    uint64_t memory_map_addr;    // Pointer to the memory map (entries described below)
-    uint64_t memory_map_entries; // Count of memory map entries
-    uint64_t framebuffer_addr;   // Address of the framebuffer and related info
+typedef struct stivale_struct {
+    char    *cmdline;
+    uint64_t memory_map_addr;
+    uint64_t memory_map_entries;
+    uint64_t framebuffer_addr;
     uint16_t framebuffer_pitch;
     uint16_t framebuffer_width;
     uint16_t framebuffer_height;
     uint16_t framebuffer_bpp;
-    uint64_t rsdp;         // Pointer to the ACPI RSDP structure
-    uint64_t module_count; // Count of modules that stivale loaded according to config
-    uint64_t modules;      // Pointer to the first entry in the linked list of modules (described below)
-    uint64_t epoch;        // UNIX epoch at boot, read from system RTC
-    uint64_t flags;        // Flags
-                           // bit 0: 1 if booted with BIOS, 0 if booted with UEFI
-                           // All other bits undefined.
-} __attribute__((packed));
+    uint64_t rsdp;
+    uint64_t module_count;
+    stivale_module_t modules[];
+} __attribute__((packed)) stivale_info_t;
+
 #endif
