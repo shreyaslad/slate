337c5887388e2dc7fd0fef8d255d857bcb3bcc54
0xqoob <slad0716@gmail.com>
2020-09-03 11:03:57 -0700

    make memcmp not-shit

--- lib/mem.c
+++ lib/mem.c
@@ -23,8 +23,9 @@ int memcmp(const void* s1, const void* s2, size_t n) {
 	const uint8_t* p2 = s2;
 
 	for (size_t i = 0; i < n; i++) {
-	if (p1[i] != p2[i])
-		return p1[i] < p2[i] ? -1 : 1;
+		if (p1[i] != p2[i]) {
+			return p1[i] < p2[i] ? -1 : 1;
+		}
 	}
 
 	return 0;
