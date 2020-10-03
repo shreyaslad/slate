35334a34d6f0a6fbd5bcf0190db3d409af658fcc
0xqoob <slad0716@gmail.com>
2020-10-02 18:34:30 -0700

    yes

--- mm/vmm.c
+++ mm/vmm.c
@@ -2,6 +2,13 @@
 
 static spinlock_t vmm_lock;
 
+struct idx_t{
+    size_t pml1idx;
+    size_t pml2idx;
+    size_t pml3idx;
+    size_t pml4idx;
+};
+
 size_t* get_pml4() {
     size_t* addr;
     asm volatile("movq %%cr3, %0;" : "=r"(addr));
@@ -20,23 +27,24 @@ void tlbflush() {
     set_pml4(get_pml4());
 }
 
-// virtual address to offset
-void vtoof(offset_t* offset, size_t* vaddr) {
+void vtoidx(struct idx_t* ret, size_t* vaddr) {
     size_t addr = (size_t)vaddr;
 
-    offset->pml4off = (addr & ((size_t)0x1ff << 39)) >> 39;
-    offset->pml3off = (addr & ((size_t)0x1ff << 30)) >> 30;
-    offset->pml2off = (addr & ((size_t)0x1ff << 30)) >> 30;
-    offset->pml1off = (addr & ((size_t)0x1ff << 12)) >> 12;
+    ret->pml4idx = (addr & ((size_t)0x1ff << 39)) >> 39;
+    ret->pml3idx = (addr & ((size_t)0x1ff << 30)) >> 30;
+    ret->pml2idx = (addr & ((size_t)0x1ff << 30)) >> 30;
+    ret->pml1idx = (addr & ((size_t)0x1ff << 12)) >> 12;
 
     return;
 }
 
 void vmm_map(size_t* vaddr, size_t* paddr, size_t* pml4ptr, size_t flags) {
     spinlock_lock(&vmm_lock);
-    
-    offset_t offset;
-    vtoof(&offset, vaddr);
+
+    asm volatile("cli");
+
+    struct idx_t indicies;
+    vtoidx(&indicies, vaddr);
 
     size_t  pml3phys = NULL;
     size_t* pml3virt = NULL;
@@ -45,47 +53,47 @@ void vmm_map(size_t* vaddr, size_t* paddr, size_t* pml4ptr, size_t flags) {
     size_t  pml1phys = NULL;
     size_t* pml1virt = NULL;
 
-    if (pml4ptr[offset.pml4off] & TABLEPRESENT) {
-        pml3phys = (size_t)(pml4ptr[offset.pml4off] & RMFLAGS);
+    if (pml4ptr[indicies.pml4idx] & TABLEPRESENT) {
+        pml3phys = (size_t)(pml4ptr[indicies.pml4idx] & RMFLAGS);
         pml3virt = (size_t)(pml3phys + HIGH_VMA);
     } else {
         pml3phys = pmm_alloc(1);
         memset(pml3phys + HIGH_VMA, 0, PAGESIZE);
         pml3virt = (size_t)(pml3phys + HIGH_VMA);
-        pml4ptr[offset.pml4off] = (size_t)pml3phys | flags;
+        pml4ptr[indicies.pml4idx] = (size_t)pml3phys | flags;
     }
 
-    if (pml3virt[offset.pml3off] & TABLEPRESENT) {
-        pml2phys = (size_t)(pml3virt[offset.pml3off] & RMFLAGS);
+    if (pml3virt[indicies.pml3idx] & TABLEPRESENT) {
+        pml2phys = (size_t)(pml3virt[indicies.pml3idx] & RMFLAGS);
         pml2virt = (size_t)(pml2phys + HIGH_VMA);
     } else {
         pml2phys = pmm_alloc(1);
         memset(pml2phys + HIGH_VMA, 0, PAGESIZE);
         pml2virt = (size_t)(pml2phys + HIGH_VMA);
-        pml3virt[offset.pml3off] = (size_t)pml2phys | flags;
+        pml3virt[indicies.pml3idx] = (size_t)pml2phys | flags;
     }
 
-    if (pml2virt[offset.pml2off] & TABLEPRESENT) {
-        pml1phys = (size_t)(pml2virt[offset.pml2off] & RMFLAGS);
+    if (pml2virt[indicies.pml2idx] & TABLEPRESENT) {
+        pml1phys = (size_t)(pml2virt[indicies.pml2idx] & RMFLAGS);
         pml1virt = (size_t)(pml1phys + HIGH_VMA);
     } else {
         pml1phys = pmm_alloc(1);
         memset(pml1phys + HIGH_VMA, 0, PAGESIZE);
         pml1virt = (size_t)(pml1phys + HIGH_VMA);
-        pml2virt[offset.pml2off] = (size_t)pml1phys | flags;
+        pml2virt[indicies.pml2idx] = (size_t)pml1phys | flags;
     }
 
-    pml1virt[offset.pml1off] = (size_t)paddr | flags;
+    pml1virt[indicies.pml1idx] = (size_t)paddr | flags;
 
     invlpg(vaddr);
 
-    //asm volatile("sti");
+    asm volatile("sti");
     spinlock_release(&vmm_lock);
 
     return;
 }
 
-void vmm_free(size_t* vaddr, size_t pages) {
+void vmm_unmap(size_t* vaddr, size_t pages) {
     spinlock_lock(&vmm_lock);
     asm volatile("cli");
     
--- mm/vmm.h
+++ mm/vmm.h
@@ -8,23 +8,13 @@
 #include <mm/pmm.h>
 #include <str.h>
 
-typedef struct {
-    size_t pml1off;
-    size_t pml2off;
-    size_t pml3off;
-    size_t pml4off;
-} offset_t;
-
 uint64_t* get_pml4();
 void set_pml4(uint64_t pml4);
 void invlpg(uint64_t* vaddr);
 void tlbflush();
 
-uint64_t* getpaddr(void* vaddr);
-void vtoof(offset_t* offset, uint64_t* vaddr); // virtual address to offset
-
 void vmm_map(uint64_t* vaddr, uint64_t* paddr, uint64_t* pml4ptr, uint64_t permission);
-void vfree(uint64_t* vaddr, size_t pages);
+void vmm_unmap(uint64_t* vaddr, size_t pages);
 
 void test();
 
